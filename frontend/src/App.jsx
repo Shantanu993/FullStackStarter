c6d570724533f1de0bd306be326581ffc7a35e8a
Shantanu993 <shantanugahtori4@gmail.com>
2024-01-22 22:00:55 +0530

    Add axios dependency and fetch jokes from API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [jokes, setJokes] = useState([])
+
+  useEffect(() => {
+    axios.get('https://localhost:3000/jokes')
+    .then((response)=>{
+      setJokes(response.data)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  })
 
   return (
     <>
       <h1>basic full stack application</h1>
+      <p>Jokes:{jokes.length}</p>
+
+      {jokes.map((joke)=>{
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div> 
+      })}
     </>
   )
 }

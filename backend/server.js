699ce028d0e207836aa82f0895222e8317189588
Shantanu993 <shantanugahtori4@gmail.com>
2024-01-22 10:51:58 +0530

    created a simple api

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,25 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+//     });
+
+
+// get a list of 5 jokes
+
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        { id: 1, title: 'Joke 1', content: 'This is joke 1' },
+        { id: 2, title: 'Joke 2', content: 'This is joke 2' },
+        { id: 3, title: 'Joke 3', content: 'This is joke 3' },
+        { id: 4, title: 'Joke 4', content: 'This is joke 4' },
+        { id: 5, title: 'Joke 5', content: 'This is joke 5' }
+    ];
+
+    res.send(jokes);
+})
+
 
 const port = process.env.PORT || 3000;
 
